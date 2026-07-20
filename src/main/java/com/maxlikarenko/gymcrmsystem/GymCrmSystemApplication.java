1422e8501ddbb0735f18541814430aef5131b05d
Max Likarenko <maxlikarenko@gmail.com>
2026-07-20 17:14:51 +0200

    Initial commit

---
+++ src/main/java/com/maxlikarenko/gymcrmsystem/GymCrmSystemApplication.java
@@ -0,0 +1,13 @@
+package com.maxlikarenko.gymcrmsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymCrmSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymCrmSystemApplication.class, args);
+    }
+
+}
