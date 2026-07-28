b0621cabcf7cd9f79f15d5d2d1c8553d884607cc
Max Likarenko <maxlikarenko@gmail.com>
2026-07-28 21:31:51 +0200

    refactor: redesign repository layer for JPA

--- src/main/java/com/maxlikarenko/gymcrmsystem/repository/TraineeRepository.java
+++ src/main/java/com/maxlikarenko/gymcrmsystem/repository/TraineeRepository.java
@@ -1,12 +1,10 @@
 package com.maxlikarenko.gymcrmsystem.repository;
 
 import com.maxlikarenko.gymcrmsystem.model.Trainee;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface TraineeRepository {
-    Trainee save(Trainee trainee);
-    void deleteById(Long id);
-    Optional<Trainee> findById(Long id);
-    boolean existsByUsername(String username);
+public interface TraineeRepository extends JpaRepository<Trainee, Long> {
+    Optional<Trainee> findByUserUsername(String username);
 }
--- src/main/java/com/maxlikarenko/gymcrmsystem/repository/TrainingRepository.java
+++ src/main/java/com/maxlikarenko/gymcrmsystem/repository/TrainingRepository.java
@@ -1,10 +1,31 @@
 package com.maxlikarenko.gymcrmsystem.repository;
 
 import com.maxlikarenko.gymcrmsystem.model.Training;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import java.time.LocalDate;
+import java.util.Set;
 
-public interface TrainingRepository {
-    Training save(Training training);
-    Optional<Training> findById(Long id);
+public interface TrainingRepository extends JpaRepository<Training, Long> {
+    @Query("""
+        SELECT t FROM Training t
+        WHERE t.trainee.user.username = :traineeUsername
+            AND (:fromDate IS NULL OR t.date >= :fromDate)
+            AND (:toDate IS NULL OR t.date <= :toDate)
+            AND (:trainerUsername IS NULL OR t.trainer.user.username = :trainerUsername)
+            AND (:trainingTypeName IS NULL OR t.type.name = :trainingTypeName)
+    """)
+    Set<Training> findTraineeTrainings(String traineeUsername, LocalDate fromDate, LocalDate toDate,
+                                       String trainerUsername, String trainingTypeName);
+
+    @Query("""
+        SELECT t FROM Training t
+        WHERE t.trainer.user.username = :trainerUsername
+            AND (:fromDate IS NULL OR t.date >= :fromDate)
+            AND (:toDate IS NULL OR t.date <= :toDate)
+            AND (:traineeUsername IS NULL OR t.trainee.user.username = :traineeUsername)
+    """)
+    Set<Training> findTrainerTrainings(String trainerUsername, LocalDate fromDate, LocalDate toDate,
+                                        String traineeUsername);
 }
